7e2c156ca260e358439c199400a750e1520f36fe
João Marcos <134110271+joaomarcosvs@users.noreply.github.com>
2025-04-09 14:37:38 -0300

    Add files via upload

---
+++ desafiocurso4/src/application/Program.java
@@ -0,0 +1,20 @@
+package application;
+
+import entities.Rent;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rent r = new Rent("Maria", "mariagz@gmail.com");
+
+        System.out.println(r);
+
+        sc.close();
+    }
+}
